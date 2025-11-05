40fefaf9f74c8f24bc8ee4ad022f10d0461d9986
Vincenzo Sarnataro <v.sarnataro@dstech.it>
2025-11-05 18:40:00 +0100

    new release

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "edefb86fc4516966cc1735c8b92d50df",
+const RESOURCES = {"flutter_bootstrap.js": "47d1f69f43ea9ce6f0c1d9d23db2e253",
 "version.json": "902e40b1d4c74e2261d33c7a57584dcc",
 "index.html": "53a9e64f7943e2152351242117952a56",
 "/": "53a9e64f7943e2152351242117952a56",
-"main.dart.js": "7d23186b57e01564a7d262a8145758f7",
+"main.dart.js": "fa2b3c7054cbf3c2b4abef9acb2c6456",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "37d995373cc78b6ec65069879362bd1a",
 "icons/Icon-192.png": "9f77000106de35b32678cf2a1503ca8e",
